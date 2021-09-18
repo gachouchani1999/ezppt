d3407714dcb0d4a431603b0adf47acbf89ed3d53
byron-harris <bbh3@rice.edu>
2021-09-18 01:09:08 -0500

    extremely simple UI, single file upload

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,6 +1,6 @@
 import os
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_bootstrap import Bootstrap
 
 
@@ -8,8 +8,10 @@ def create_app(test_config=None):
 
     app = Flask(__name__, instance_relative_config=True)
 
-    @app.route('/')
-    def hello():
+    @app.route('/', methods=('GET', 'POST'))
+    def index():
+        if request.method == 'POST':
+            pass
         return render_template('index.html')
 
     Bootstrap(app)
