4f496168103c90ac08abd82882d57a008c19db4d
unknown <georgeschouchani1@gmail.com>
2021-09-19 05:28:14 +0300

    nearly final version

--- flaskr/static/download.js
+++ flaskr/static/download.js
@@ -3,12 +3,11 @@ document.addEventListener('DOMContentLoaded', function() {
 });
 
 function changeDisplay() {
-    if (Math.random() > 0.5) {
+   {
         displaySuccess()
-    } else {
-        displayFailure()
-    }
-}
+   }
+   
+
 
 function displaySuccess() {
     document.getElementById("loading").style.display = "none";
@@ -17,9 +16,4 @@ function displaySuccess() {
     document.getElementById("pptx-status").innerHTML = "All done, click below to get your powerpoint!"
 }
 
-function displayFailure() {
-    document.getElementById("loading").style.display = "none";
-    document.getElementById("failure").style.display = "block";
-    document.getElementById("restartBtn").style.display = "inline";
-    document.getElementById("pptx-status").innerHTML = "Hmm, looks like something went wrong..."
 }
