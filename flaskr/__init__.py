5896d6094b159056f083e84db43c69a20684b1b2
unknown <georgeschouchani1@gmail.com>
2021-09-19 13:59:36 +0300

    Threaded fix

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -40,4 +40,4 @@ Bootstrap (app)
 
 if __name__ == '__main__':
     # Threaded option to enable multiple instances for multiple user access support
-    app.run(threaded=True)
+    app.run(threaded=False)
